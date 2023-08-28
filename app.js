504c3a4c013eb4a4fa3a34a46a58c04ad47a82b6
trannghia91vn <trannghia213@gmail.com>
2023-08-29 02:03:31 +0700

    Bật cái trust proxy lên

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.use(compression());
 app.use(express.json({ limit: "10kb" }));
 
 //Giới hạn req data từ một user ip
-// app.set("trust proxy", true);
+app.set("trust proxy", true);
 const rateLimit = require("express-rate-limit");
 const myRateLimiting = rateLimit({
   //Max req trong khoảng thời gian
